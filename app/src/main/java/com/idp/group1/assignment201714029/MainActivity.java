12344155c00f3dd4d159a5d57dadfce249176e5b
wazedrifat <34921453+wazedrifat@users.noreply.github.com>
2019-09-27 21:54:50 +0600

    nav drawer

--- app/src/main/java/com/idp/group1/assignment201714029/MainActivity.java
+++ app/src/main/java/com/idp/group1/assignment201714029/MainActivity.java
@@ -29,22 +29,17 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
 		Toolbar toolbar = findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
-		FloatingActionButton fab = findViewById(R.id.fab);
-		fab.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-						.setAction("Action", null).show();
-			}
-		});
+
 		DrawerLayout drawer = findViewById(R.id.drawer_layout);
 		NavigationView navigationView = findViewById(R.id.nav_view);
 		// Passing each menu ID as a set of Ids because each
 		// menu should be considered as top level destinations.
 		mAppBarConfiguration = new AppBarConfiguration.Builder(
-				)
+				R.id.nav_Accelerometer, R.id.nav_Gyroscope,
+				R.id.nav_Proximity, R.id.nav_ShakeDetection)
 				.setDrawerLayout(drawer)
 				.build();
 		NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
