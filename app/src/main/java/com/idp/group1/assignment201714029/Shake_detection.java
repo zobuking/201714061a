9431ccbddcf6ab8ea986976f9f87496d092a1511
wazedrifat <34921453+wazedrifat@users.noreply.github.com>
2019-09-27 21:27:13 +0600

    Initial commit

---
+++ app/src/main/java/com/idp/group1/assignment201714029/Shake_detection.java
@@ -0,0 +1,31 @@
+package com.idp.group1.assignment201714029;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Shake_detection extends Fragment {
+
+
+	public Shake_detection() {
+		// Required empty public constructor
+	}
+
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+							 Bundle savedInstanceState) {
+		// Inflate the layout for this fragment
+		return inflater.inflate(R.layout.fragment_shake_detection, container, false);
+	}
+
+}
