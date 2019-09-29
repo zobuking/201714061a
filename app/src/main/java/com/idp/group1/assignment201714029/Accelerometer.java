e4171aba448a3e88ca396c9c9eb23787e488f2be
wazedrifat <34921453+wazedrifat@users.noreply.github.com>
2019-09-30 03:08:40 +0600

    accelerometer

--- app/src/main/java/com/idp/group1/assignment201714029/Accelerometer.java
+++ app/src/main/java/com/idp/group1/assignment201714029/Accelerometer.java
@@ -1,6 +1,11 @@
 package com.idp.group1.assignment201714029;
 
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,13 +13,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class Accelerometer extends Fragment {
+public class Accelerometer extends Fragment implements SensorEventListener {
 
+	private TextView xAxisText, yAxisText, zAxisText;
 
 	public Accelerometer() {
 		// Required empty public constructor
@@ -22,10 +29,46 @@ public class Accelerometer extends Fragment {
 
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-							 Bundle savedInstanceState) {
-		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_accelerometer, container, false);
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 	}
 
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+							 Bundle savedInstanceState) {
+
+		View view = inflater.inflate(R.layout.fragment_accelerometer, container, false);
+
+		SensorManager sensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
+		Sensor accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+		sensorManager.registerListener(this, accelerometer, sensorManager.SENSOR_DELAY_NORMAL);
+
+		xAxisText = (TextView) view.findViewById(R.id.xAxisID);
+		yAxisText = (TextView) view.findViewById(R.id.yAxisID);
+		zAxisText = (TextView) view.findViewById(R.id.zAxisID);
+
+		return view;
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent sensorEvent) {
+		double x = (double) sensorEvent.values[0];
+		double y = (double) sensorEvent.values[1];
+		double z = (double) sensorEvent.values[2];
+
+		x = Math.round(x * 1000.0) / 1000.0;
+		y = Math.round(y * 1000.0) / 1000.0;
+		z = Math.round(z * 1000.0) / 1000.0;
+
+		xAxisText.setText(String.valueOf(x));
+		yAxisText.setText(String.valueOf(y));
+		zAxisText.setText(String.valueOf(z));
+
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int i) {
+
+	}
 }
