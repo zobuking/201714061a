cdf519c89920a60aa88c983e3152bf6f0abdd82c
wazedrifat <34921453+wazedrifat@users.noreply.github.com>
2019-10-05 21:20:12 +0600

    floating button

--- app/src/main/java/com/idp/group1/assignment201714029/map.java
+++ app/src/main/java/com/idp/group1/assignment201714029/map.java
@@ -1,6 +1,7 @@
 package com.idp.group1.assignment201714029;
 
 
+import android.location.Location;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -10,13 +11,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
 /**
@@ -25,6 +31,9 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class map extends Fragment implements OnMapReadyCallback {
 
 	private GoogleMap mMap;
+	private FloatingActionButton floatingActionButton;
+	private FusedLocationProviderClient fusedLocationProviderClient;
+	private double latitude = -34.0, longitude = 151.0;
 
 	public map() {
 		// Required empty public constructor
@@ -35,7 +44,37 @@ public class map extends Fragment implements OnMapReadyCallback {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 							 Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_map, container, false);
+		View view =  inflater.inflate(R.layout.fragment_map, container, false);
+
+		fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
+
+		floatingActionButton = view.findViewById(R.id.floatingButtonID);
+		floatingActionButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				getLocation();
+			}
+		});
+
+		return view;
+	}
+
+	private void getLocation() {
+		Toast.makeText(getContext(), "button Clicked", Toast.LENGTH_SHORT).show();
+		fusedLocationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+			@Override
+			public void onSuccess(Location location) {
+				if (location != null) {
+					latitude = location.getLatitude();
+					longitude = location.getLongitude();
+
+					Toast.makeText(getContext(), latitude + " " + longitude, Toast.LENGTH_SHORT).show();
+				}
+				else {
+					Toast.makeText(getContext(), "location is null", Toast.LENGTH_SHORT).show();
+				}
+			}
+		});
 	}
 
 	@Override
@@ -51,7 +90,7 @@ public class map extends Fragment implements OnMapReadyCallback {
 		mMap = googleMap;
 
 		// Add a marker in Sydney and move the camera
-		LatLng sydney = new LatLng(-34, 151);
+		LatLng sydney = new LatLng(latitude, longitude);
 		mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
 		mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 	}
